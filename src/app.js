8d303c9ae5352dea9d9b6d70bf83c8e908f82cc5
Eduardo Grajeda <tatofoo@gmail.com>
2011-11-18 11:26:52 -0300

    Adding sounds for connection and messages

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
-var app = require('express').createServer()
-  , io  = require("socket.io").listen(app);
+var app     = require('express').createServer()
+  , express = require('express')
+  , io      = require("socket.io").listen(app);
 
 app.listen(8080);
 
-app.get("/", function (req, res) {
-    res.sendfile(__dirname + "/index.html");
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/index.html');
+});
+app.get('/sounds/message.wav', function (req, res) {
+    res.sendfile(__dirname + '/sounds/message.wav');
+});
+app.get('/sounds/connection.wav', function (req, res) {
+    res.sendfile(__dirname + '/sounds/connection.wav');
 });
 
 io.sockets.on('connection', function(socket) {
+    socket.broadcast.emit('connection');
+
     socket.on('message', function(data) {
         socket.emit('message', { message: data.message });
         socket.broadcast.emit('message', { message: data.message });
